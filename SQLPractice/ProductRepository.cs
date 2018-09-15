1a6634e6960b88fdfaf54a6421e2a6d814471ca7
stbrown <shadowtuner1@gmail.com>
2018-09-14 19:59:49 -0500

    Filled out get products method to retrieve the name, id, and price of the products from the product table

--- SQLPractice/ProductRepository.cs
+++ SQLPractice/ProductRepository.cs
@@ -1,10 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
 namespace SQLPractice
 {
     public class ProductRepository
     {
-        public ProductRepository()
+        public string connectionString = "Server=127.0.0.1;Database=adventureworks;Uid=root;Pwd=password;";
+
+        public List<Product> GetProducts()
         {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+
+            using (conn)
+            {
+                conn.Open();
+
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = "SELECT ProductID AS id, name, ListPrice AS price FROM product;";
+                MySqlDataReader reader = cmd.ExecuteReader();
+
+                List<Product> products = new List<Product>();
+                while(reader.Read())
+                {
+                    Product prod = new Product();
+                    prod.Id = (int)reader["id"];
+                    prod.Name = reader["name"].ToString();
+                    prod.Price = (double)reader["price"];
+                    products.Add(prod);
+                }
+
+                return products;
+            }
+
         }
+
     }
 }
--- SQLPractice/Program.cs
+++ SQLPractice/Program.cs
@@ -6,7 +6,16 @@ namespace SQLPractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var repo = new ProductRepository();
+
+            var list1 = repo.GetProducts();
+
+            foreach(Product prods in list1)
+            {
+                Console.WriteLine($"{prods.Id} {prods.Name} ------- ${prods.Price}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
